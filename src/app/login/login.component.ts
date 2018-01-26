39697272163bf0fd4ab24c2e1cd57683843ee093
Jaime Crespo <jcrespoas@gmail.com>
2018-01-26 01:12:52 +0100

    dev

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -35,6 +35,7 @@ export class LoginComponent implements OnInit {
                     this.router.navigate([this.returnUrl]);
                 },
                 error => {
+                    console.log(error);
                     this.alertService.error(error);
                     this.loading = false;
                 });
